73e5ea1169bec9f6ffb90e69d91f2000b4d3c691
Ivan Vorobei <evdevosh@gmail.com>
2025-04-07 11:04:04 +0200

    5/

--- src/components/BattleField.vue
+++ src/components/BattleField.vue
@@ -42,7 +42,7 @@
 export default {
   data() {
     return {
-      iframeSrc: 'src/assets/public/index.html',
+      iframeSrc: '/Lost-in-the-Tiles/src/assets/public/index.html',
       isLoading: true, // Состояние загрузки
     };
   },

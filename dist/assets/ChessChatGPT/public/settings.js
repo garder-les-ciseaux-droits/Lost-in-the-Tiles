ab1ad4d06e1b7e32b5aa866c7ab6ce08a3fd5e8d
Ivan Vorobei <evdevosh@gmail.com>
2025-04-07 10:08:02 +0200

    removing engine

--- dist/assets/ChessChatGPT/public/settings.js
+++
@@ -1,9 +0,0 @@
-const settingsBtn = document.getElementById('settings');
-
-let isSettingsOpen = false;
-
-settingsBtn.addEventListener('click', function(){
-    if (!isSettingsOpen){
-        isSettingsOpen = true;
-    }
-})
